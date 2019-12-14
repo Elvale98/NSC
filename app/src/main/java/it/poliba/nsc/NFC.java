f614f3ba49337d648a9ce5ed63a4512ed184fd98
Antonello <58037244+Elvale98@users.noreply.github.com>
2019-12-14 12:57:47 +0100

    Update NFC.java

--- app/src/main/java/it/poliba/nsc/NFC.java
+++ app/src/main/java/it/poliba/nsc/NFC.java
@@ -4,49 +4,175 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
-import android.nfc.tech.MifareUltralight;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
 import android.os.Bundle;
 import android.util.Log;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import android.nfc.Tag;
-import android.nfc.tech.MifareUltralight;
-import android.util.Log;
-import java.io.IOException;
-import java.nio.charset.Charset;
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class NFC extends AppCompatActivity {
+
+    NfcAdapter nfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(
-                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if(nfcAdapter != null && nfcAdapter.isEnabled()){
+
+        }else{
+            finish();
+        }
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
     }
 
-    private static final String TAG = NFC.class.getSimpleName();
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-    public void writeTag(Tag tag, String tagText) {
-        MifareUltralight ultralight = MifareUltralight.get(tag);
-        try {
-            ultralight.connect();
-            ultralight.writePage(4, "abcd".getBytes(Charset.forName("US-ASCII")));
-            ultralight.writePage(5, "efgh".getBytes(Charset.forName("US-ASCII")));
-            ultralight.writePage(6, "ijkl".getBytes(Charset.forName("US-ASCII")));
-            ultralight.writePage(7, "mnop".getBytes(Charset.forName("US-ASCII")));
-        } catch (IOException e) {
-            Log.e(TAG, "IOException while writing MifareUltralight...", e);
-        } finally {
-            try {
-                ultralight.close();
-            } catch (IOException e) {
-                Log.e(TAG, "IOException while closing MifareUltralight...", e);
-            }
+        enableForegroundDispatchSystem();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        disableForegroundDispatchSystem();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        if (intent.hasExtra(NfcAdapter.EXTRA_TAG)) {
+            Toast.makeText(this, "NfcIntent!", Toast.LENGTH_SHORT).show();
+
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            NdefMessage ndefMessage = createNdefMessage("Il contenuto della mia stringa!");
+
+            writeNdefMessage(tag, ndefMessage);
         }
     }
+
+    private void enableForegroundDispatchSystem() {
+
+        Intent intent = new Intent(this, MainActivity.class).addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        IntentFilter[] intentFilters = new IntentFilter[]{};
+
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters, null);
+    }
+
+    private void disableForegroundDispatchSystem() {
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    private void formatTag(Tag tag, NdefMessage ndefMessage) {
+        try {
+
+            NdefFormatable ndefFormatable = NdefFormatable.get(tag);
+
+            if (ndefFormatable == null) {
+                Toast.makeText(this, "Tag non formattabile!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+
+            ndefFormatable.connect();
+            ndefFormatable.format(ndefMessage);
+            ndefFormatable.close();
+
+            Toast.makeText(this, "Tag scrivibile!", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            Log.e("formatTag", e.getMessage());
+        }
+
+    }
+
+    private void writeNdefMessage(Tag tag, NdefMessage ndefMessage) {
+
+        try {
+
+            if (tag == null) {
+                Toast.makeText(this, "L'oggetto Tag non può essere nullo", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Ndef ndef = Ndef.get(tag);
+
+            if (ndef == null) {
+                // format tag with the ndef format and writes the message.
+                formatTag(tag, ndefMessage);
+            } else {
+                ndef.connect();
+
+                if (!ndef.isWritable()) {
+                    Toast.makeText(this, "Tag non scrivibile!", Toast.LENGTH_SHORT).show();
+
+                    ndef.close();
+                    return;
+                }
+
+                ndef.writeNdefMessage(ndefMessage);
+                ndef.close();
+
+                Toast.makeText(this, "Tag scritto!", Toast.LENGTH_SHORT).show();
+
+            }
+
+        } catch (Exception e) {
+            Log.e("writeNdefMessage", e.getMessage());
+        }
+
+    }
+
+
+    private NdefRecord createTextRecord(String content) {
+        try {
+            byte[] language;
+            language = Locale.getDefault().getLanguage().getBytes("UTF-8");
+
+            final byte[] text = content.getBytes("UTF-8");
+            final int languageSize = language.length;
+            final int textLength = text.length;
+            final ByteArrayOutputStream payload = new ByteArrayOutputStream(1 + languageSize + textLength);
+
+            payload.write((byte) (languageSize & 0x1F));
+            payload.write(language, 0, languageSize);
+            payload.write(text, 0, textLength);
+
+            return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], payload.toByteArray());
+
+        } catch (UnsupportedEncodingException e) {
+            Log.e("createTextRecord", e.getMessage());
+        }
+        return null;
+    }
+
+
+    private NdefMessage createNdefMessage(String content) {
+
+        NdefRecord ndefRecord = createTextRecord(content);
+
+        NdefMessage ndefMessage = new NdefMessage(new NdefRecord[]{ndefRecord});
+
+        return ndefMessage;
+    }
+
 }
