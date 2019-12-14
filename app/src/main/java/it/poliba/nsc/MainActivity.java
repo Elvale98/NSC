a18990fa477a0de2818bcdb97e8d12453a8c60f2
Antonello <58037244+Elvale98@users.noreply.github.com>
2019-12-14 18:18:13 +0100

    Update MainActivity.java

--- app/src/main/java/it/poliba/nsc/MainActivity.java
+++ app/src/main/java/it/poliba/nsc/MainActivity.java
@@ -3,9 +3,7 @@ package it.poliba.nsc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.nfc.NfcAdapter;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,10 +13,7 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.net.NetworkInterface;
-import java.net.SocketException;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.concurrent.TimeUnit;
 
 import it.poliba.nsc.dboperation.VerificaAccoutBackground;
