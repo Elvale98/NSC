251eb249b4385faead200ee60046c8ff4fd4b7d8
Antonello <58037244+Elvale98@users.noreply.github.com>
2019-12-14 12:31:49 +0100

    Update MainActivity.java

--- app/src/main/java/it/poliba/nsc/MainActivity.java
+++ app/src/main/java/it/poliba/nsc/MainActivity.java
@@ -119,23 +119,7 @@ public class MainActivity extends AppCompatActivity {
             writeNdefMessage(tag, ndefMessage);
         }
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
+    
     private void enableForegroundDispatchSystem() {
 
         Intent intent = new Intent(this, MainActivity.class).addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
